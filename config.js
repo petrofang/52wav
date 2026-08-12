c64788e3e9918dbda122e30e866d5eb5fc16d4ff
Petrofang <giles.cooper@gmail.com>
2026-08-12 09:23:26 -0400

    Serve peak data from the published Gist, with the bundled copy as fallback

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // Point DATA_URL at the raw URL of your public Gist's peaks.json.
 // Use the "raw" link WITHOUT the commit SHA so edits to the Gist show up automatically:
-//   https://gist.githubusercontent.com/Petrofang/<GIST_ID>/raw/peaks.json
+//   https://gist.githubusercontent.com/petrofang/<GIST_ID>/raw/peaks.json
 // Leave it null to use the bundled copy in data/peaks.json.
 window.APP_CONFIG = {
-  DATA_URL: null,
+  DATA_URL: 'https://gist.githubusercontent.com/petrofang/46213d7d93292f14ffd54d955b7f3f67/raw/peaks.json',
   FALLBACK_URL: 'data/peaks.json'
 };
